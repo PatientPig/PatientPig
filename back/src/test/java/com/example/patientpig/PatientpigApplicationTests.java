ff5324fcea115ca4f47e22cd32bce16158d0f461
gunh0 <devgun@kakao.com>
2022-09-03 21:00:01 +0900

    feat: build & run TS

--- back/src/test/java/com/example/patientpig/PatientpigApplicationTests.java
+++ back/src/test/java/com/example/patientpig/PatientpigApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.patientpig;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class PatientpigApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+   		assertThat(context).isNotNull();
 	}
 
 }
