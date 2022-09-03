e5e8a5da6d1298a3dcec425d112e08326e5c2e21
gimhanul <gimhanuly@gmail.com>
2022-09-04 00:14:45 +0900

    ADD :: 분기 삭ㅈ[

--- back/src/main/java/com/example/patientpig/domain/user/service/UserService.java
+++ back/src/main/java/com/example/patientpig/domain/user/service/UserService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface UserService {
     String createUserAndGetNickname();
-    Integer getPig(String nickname); // 분기 컨트롤 여기서
+    Integer getPig(String nickname);
     String updateUser(String nickname, String newNickname);
-    List<UserResponse> getPigRanking(); // User Pig response DTO
+    List<UserResponse> getPigRanking();
 }
