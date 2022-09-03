8e0440b4c5c89eb05e19ebf23c48f2d905bc2797
gimhanul <gimhanuly@gmail.com>
2022-09-03 21:52:10 +0900

    DELETE :: log

--- back/src/main/java/com/example/patientpig/domain/user/service/UserServiceImpl.java
+++ back/src/main/java/com/example/patientpig/domain/user/service/UserServiceImpl.java
@@ -1,10 +1,9 @@
 package com.example.patientpig.domain.user.service;
 
 import com.example.patientpig.domain.user.domain.User;
-import com.example.patientpig.domain.user.presentation.dto.UserResponse;
 import com.example.patientpig.domain.user.domain.repository.UserRepository;
+import com.example.patientpig.domain.user.presentation.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -46,7 +44,6 @@ public class UserServiceImpl implements UserService {
     public Integer getPig(String nickname) { // 분기 컨트롤 여기서 (테스트하며 한계값 수정)
         double pig = userRepository.findByNickname(nickname).getPig();
 
-        log.info("==================> " + LEVEL_1 + " " + LEVEL_2 + " " + LEVEL_3);
         if (pig < LEVEL_1) {
             return 1;
         } else if (LEVEL_1 <= pig && pig < LEVEL_2) {
