c48f954a5153816588a5db72ef35f99fe70b4b5a
gimhanul <gimhanuly@gmail.com>
2022-09-04 00:06:59 +0900

    ADD :: 오름차순, 내림차순

--- back/src/main/java/com/example/patientpig/domain/user/domain/repository/UserRepository.java
+++ back/src/main/java/com/example/patientpig/domain/user/domain/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNickname(String nickname);
 
-    List<User> findAllByOrderByPig();
+    List<User> findAllByOrderByPigDesc();
 }
