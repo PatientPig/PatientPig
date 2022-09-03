c901a4438e922ad6c31dee184a711708567f8301
gunh0 <devgun@kakao.com>
2022-09-03 11:53:24 +0900

    feat: add /first GET api

---
+++ back/src/main/java/com/example/patientpig/PatientpigApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientpig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientpigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientpigApplication.class, args);
+	}
+
+}
