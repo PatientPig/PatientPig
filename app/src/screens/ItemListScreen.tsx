769d81b9c82efdf7e19acd8e945c92e9fbbf0877
d0422 <rlfehd2013@naver.com>
2022-09-03 15:41:54 +0900

    :zap: 아이템 리스트 페이지

--- app/src/screens/ItemListScreen.tsx
+++ app/src/screens/ItemListScreen.tsx
@@ -1,12 +1,58 @@
-import React, { FC } from "react";
-import { SafeAreaView, Text } from "react-native";
-
+import React, { FC, useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, View, ScrollView } from "react-native";
 import { RootStackScreenProps } from "@src/types/navigation";
-
-const ItemListScreen: FC<RootStackScreenProps<"ItemList">> = () => (
-  <SafeAreaView>
-    <Text>ItemList</Text>
-  </SafeAreaView>
-);
-
+import { getItems } from "@src/apis/ItemAPI";
+import Text from "@src/components/Text";
+import Item from "@src/interface/Item";
+import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
+const ItemListScreen: FC<RootStackScreenProps<"ItemList">> = () => {
+  const [Items, setItems] = useState<Item[]>();
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    getItems().then((Item) => setItems(Item));
+    setLoading(false);
+  }, []);
+  console.log(Items);
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <FontAwesome5 name="piggy-bank" size={45} color="#EE6983" />
+        <Text style={{ fontSize: 40 }}>잘 참았다 꿀!</Text>
+      </View>
+      <ScrollView style={styles.ItemContainer}>
+        {Items?.map((item, index) => (
+          <View style={styles.item} key={index}>
+            <AntDesign name="like2" size={24} color="#0288D1" />
+            <Text style={{ fontSize: 10 }}>{item.desc}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-evenly",
+  },
+  item: {
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 60,
+    margin: 10,
+    padding: 5,
+  },
+  ItemContainer: {
+    flex: 1,
+    flexDirection: "row",
+    margin: 10,
+    flexWrap: "wrap",
+  },
+});
 export default ItemListScreen;
