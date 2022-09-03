1dde818cb757f3ab7f8f6f791f62004b4c26da2e
gimhanul <gimhanuly@gmail.com>
2022-09-03 15:16:40 +0900

    ADD :: Coin Entity (#2)

--- back/src/main/java/com/example/patientpig/domain/coin/domain/Coin.java
+++ back/src/main/java/com/example/patientpig/domain/coin/domain/Coin.java
@@ -1,4 +1,25 @@
 package com.example.patientpig.domain.coin.domain;
 
-public class Coin {
+import com.example.patientpig.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_coin")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Coin extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime time;
 }
